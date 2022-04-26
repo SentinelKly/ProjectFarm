7c019f61ec45f96391fa4e6c620ad602cf5cea00
SentinelKly <100457265+SentinelKly@users.noreply.github.com>
2022-04-25 23:01:00 -0400

    Tomato turrets can now be placed

--- Assets/Scripts/PlacementUnit.cs
+++ Assets/Scripts/PlacementUnit.cs
@@ -1,17 +1,35 @@
-using System;
 using UnityEngine;
 
 public class PlacementUnit : MonoBehaviour
 {
+	public GameObject turret;
 	private static readonly int Color1 = Shader.PropertyToID("_Color");
 
 	private Material _plotMaterial;
 	private Color _defaultColour;
+	private GameObject _currentCrop;
+	private bool _planted;
 
 	private void Start()
 	{
 		_plotMaterial = gameObject.GetComponent<Renderer>().material;
 		_defaultColour = _plotMaterial.GetColor(Color1);
+	}
+
+	private void OnMouseDown()
+	{
+		if (!_planted)
+		{
+			_currentCrop = Instantiate(turret, transform.position, Quaternion.LookRotation(new Vector3(0, 0, 0)));
+			_currentCrop.transform.Translate(0f, 1.85f, 0f, Space.Self);
+			_planted = true;
+		}
+
+		else
+		{
+			Destroy(_currentCrop);
+			_planted = false;
+		}
 	}
 
 	private void OnMouseOver()
