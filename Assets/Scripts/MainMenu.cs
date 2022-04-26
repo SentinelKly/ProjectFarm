3781e1f664e40d9f33f108f3c9cfc1a0531fc5da
SentinelKly <100457265+SentinelKly@users.noreply.github.com>
2022-04-26 04:30:44 -0400

    Working main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+	public Button startButton, exitButton;
+
+	public void Start()
+	{
+		startButton.onClick.AddListener(() => EnterGame());
+		exitButton.onClick.AddListener(() => ExitGame());
+	}
+
+	public void EnterGame()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit();
+	}
+}
